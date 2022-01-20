a39bd10eaa7b1062fb59c4528ee4fd6ca0940b53
DannyGardner <dannygardnercg@gmail.com>
2022-01-20 15:57:26 -0600

    updated readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { Route, Routes } from 'react-router-dom';
 import Empty from './components/Empty';
 import CivDetails from './components/CivDetails';
 
-
 // const API = "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
 
 function App() {
