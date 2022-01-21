8728a35b4aa857a94b24668c4204dd6f9182c42d
DannyGardner <dannygardnercg@gmail.com>
2022-01-21 13:12:13 -0600

    css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,35 +3,53 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
-        <div className='navigation'>
-            <div className='dropdown'>
-                <div className='nav-item'>Civilizations</div>
-                <div className='dropdown-civ'>
 
-                {props.civs.map(civ => {
-                    return (
-                        <div>
-                            {civ.name}
-                        </div>
-                    )
-                })}
-                </div>
-            </div>
 
-            <div className='dropdown'>
-                <div className='nav-item'>Units</div>
-            </div>
+
+        <ul className='nav-links'>
+            <li>
+                Civilizations
+                <ul className='drop-menu'>
+                    <li>drop1</li>
+                    <li>drop1</li>
+                    <li>drop1</li>
+
+                </ul>
+                
+                </li>
+            <li>Units</li>
+            <li>Structures</li>
+            <li>Technology</li>
+        </ul>
+        // <div className='navigation'>
+        //     <div className='dropdown'>
+        //         <div className='nav-item'>Civilizations</div>
+        //         <div className='dropdown-civ'>
+
+        //         {props.civs.map(civ => {
+        //             return (
+        //                 <div>
+        //                     {civ.name}
+        //                 </div>
+        //             )
+        //         })}
+        //         </div>
+        //     </div>
+
+        //     <div className='dropdown'>
+        //         <div className='nav-item'>Units</div>
+        //     </div>
             
-            <div className='dropdown'>
-                <div className='nav-item'>Technology</div>
-            </div>
+        //     <div className='dropdown'>
+        //         <div className='nav-item'>Technology</div>
+        //     </div>
 
-            <div className='dropdown'>
-                <div className='nav-item'>Structures</div>
-            </div>
+        //     <div className='dropdown'>
+        //         <div className='nav-item'>Structures</div>
+        //     </div>
 
 
-        </div>
+        // </div>
     )
 }
 
