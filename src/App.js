298fcb24779da86b22ad26438fcd289770537916
DannyGardner <dannygardnercg@gmail.com>
2022-01-21 13:58:59 -0600

    changed API url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Search from './components/Search';
 import Results from './components/Results';
 import { Route, Routes } from 'react-router-dom';
 import Empty from './components/Empty';
@@ -12,8 +11,9 @@ import Header from './components/Header';
 
 function App() {
   const [civs, setCivs] = useState([])
-  // const url = "https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
-  const url = '/civs.json'
+  const url = "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
+  // const url = '/civs.json'
+
   useEffect(()=> {
     fetch(url)
     .then((res) => res.json())
